58d2fe757a2866122ad3ad6c0d028b78cff27141
hekung <m18720991611@163.com>
2020-03-23 10:16:18 +0800

    webpack4 ts 基本项目配置

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// import * as _ from "lodash";
+
+function component() {
+    const element = document.createElement('div');
+
+    // element.innerHTML = _.join(['Hello', 'ts'], ' ');
+    element.innerHTML = "TSXSSSSSSSSSSSS"
+
+    return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.common.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        moduleIds: 'hashed',
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            },
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin()
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, "src"),
+                loader: "babel-loader"
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    stats: {
+        children: false
+    },
+};
